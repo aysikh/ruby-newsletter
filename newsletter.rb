8c3dd9405b68bfe4891b836298c752c10d33f36f
aysikh <amy.sikhammountry@gmail.com>
2020-08-25 12:53:56 -0400

    Completed whole newsletter

--- newsletter.rb
+++ newsletter.rb
@@ -3,7 +3,6 @@ require 'pry'
 #########################
 # Data for the newsletter
 #########################
-require "pry"
 CAMPUS = {
   "name": "DC",
   "address": "1440 G St NW, Washington, DC 20005",
@@ -28,8 +27,6 @@ ARTICLES = [
 #########################
 
 def calculate_recipients
-  # Using the SUBSCRIBERS and UNSUBSCRIBED arrays,
-  # write a method that will return an array of only the subscribers who haven't unsubscribed
   SUBSCRIBERS.delete_at(1)
   SUBSCRIBERS.delete_at(3)
   SUBSCRIBERS
@@ -41,26 +38,25 @@ def first_n_articles(number_of_articles)
 end
 
 def print_recipients
-  # Write a method that uses the output of calculate_recipients
-  # and returns a list of emails separated by commas
-  # Ex) "abc@email.com, def@email.com, ghi@email.com"
+  calculate_recipients.each {|subscribers| puts subscribers + (",")}
 end
 
 def print_one_article(article)
-  # Write a method that will take an article hash
-  # and print the title, author and text as a formatted string
-  # See the README/sample output for examples
+  puts article[:title]
+  puts "by: " + article[:author]
+  puts article[:text]
 end
 
 def print_many_articles(articles)
-  # Write a method that will take in an array of article hashes
-  # and format each one using the print_one_article method
+  articles.each {|article| 
+  print_one_article(article)
+  puts " "}
+
 end
 
 def format_campus_location(campus)
   "Flatiron #{campus[:name]}"
 end
-# puts(format_campus_location(CAMPUS))
 
 def format_subject
   puts "#{format_campus_location(CAMPUS)} Newsletter - #{DATE}\n\n"
@@ -88,11 +84,7 @@ def print_newsletter(number)
 end
 
 def run
-  # We want our program to print three articles by default,
-  # but we can change that number here
   print_newsletter("3")
 end
 
-# When we run "ruby newsletter.rb" in the command line,
-# the 'run' method will be called because we're calling it below.
 run
